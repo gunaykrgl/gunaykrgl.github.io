76a042f1ad101f657c4fbd34c50767e2129831ec
Günay <40771904+gunaykrgl@users.noreply.github.com>
2022-08-16 12:57:32 +0300

    Add files via upload

--- src/data.js
+++ src/data.js
@@ -1,23 +1,14 @@
 // Skills icons - https://react-icons.github.io/react-icons/
 import {
+  FaHtml5,
+  FaCss3Alt,
+  FaBootstrap,
+  FaReact,
   FaGitAlt,
   FaGithubSquare,
   FaLink,
-  
-  
-  FaPython,
-  FaNumpy
 } from "react-icons/fa";
-
-import {
-	SiPandas,
-	SiNumpy,
-	SiTensorflow,
-	SiKeras,
-	SiJavascript,
-	SiJava
-
-} from "react-icons/si";
+import { SiJavascript, SiStyledcomponents, SiRedux } from "react-icons/si";
 
 // Projects Images (add your images to the images directory and import below)
 import Logo from "./images/logo.svg";
@@ -52,49 +43,49 @@ export const moreInfo =
 export const skillData = [
   {
     id: 1,
-    skill: <FaPython className="display-4" />,
-    name: "Python",
+    skill: <FaHtml5 className="display-4" />,
+    name: "HTML5",
   },
   {
     id: 2,
-    skill: <SiPandas className="display-4" />,
-    name: "Pandas",
+    skill: <FaCss3Alt className="display-4" />,
+    name: "CSS3",
   },
   {
     id: 3,
-    skill: <SiNumpy className="display-4" />,
-    name: "Numpy",
-  },
-  {
-    id: 4,
-    skill: <SiTensorflow className="display-4" />,
-    name: "Tensorflow",
-  },
-  {
-    id: 5,
-    skill: <SiKeras className="display-4" />,
-    name: "Keras",
-  },
-  {
-    id: 6,
     skill: <SiJavascript className="display-4" />,
     name: "JavaScript",
   },
   {
+    id: 4,
+    skill: <FaBootstrap className="display-4" />,
+    name: "BootStrap",
+  },
+  {
+    id: 5,
+    skill: <FaReact className="display-4" />,
+    name: "React",
+  },
+  {
+    id: 6,
+    skill: <SiStyledcomponents className="display-4" />,
+    name: "Styled Components",
+  },
+  {
     id: 7,
+    skill: <SiRedux className="display-4" />,
+    name: "Redux",
+  },
+  {
+    id: 8,
     skill: <FaGitAlt className="display-4" />,
     name: "Git",
   },
   {
-    id: 8,
+    id: 9,
     skill: <FaGithubSquare className="display-4" />,
     name: "GitHub",
   },
-    {
-    id: 8,
-    skill: <SiJava className="display-4" />,
-    name: "Java",
-  }
 ];
 
 // Resume link (string - "https://YourResumeUrl") - I am using CloudFront to share my resume (https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/Introduction.html)
@@ -119,4 +110,4 @@ export const projectCardImages = [
   Add your formspree endpoint below.
   https://formspree.io/
 */
-export const formspreeUrl = "https://formspree.io/f/xaykydwq";
+export const formspreeUrl = "https://formspree.io/f/YourEndpoint";
